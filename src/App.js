65047c88f2c0c32ec93944bb55a0c847a2cd1e4c
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-07 21:16:42 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const postRegister = async () => {
+    try {
+      const response = await axios.post('https://localhost:7290/register', {
+        "email": "user@user.com",
+        "password": "pass"
+      }) 
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <div
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          <button onClick={postRegister}> Learn React </button>
+          
+        </div>
       </header>
     </div>
   );
