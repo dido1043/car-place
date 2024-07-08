3b5045e2b7f5891005a53acb9b35622ffc85a18f
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-08 19:28:48 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
+
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const postRegister = async () => {
+
     try {
-      const response = await axios.post('https://localhost:7290/register', {
-        "email": "user@user.com",
-        "password": "pass"
-      }) 
+
+      const response = await fetch('https://localhost:7290/register',
+        {
+          method: 'POST',
+          headers: { 
+           'Content-Type': 'application/json',
+           'Accept': '*/*' 
+          },
+          body: {
+            email: 'user@user.com',
+            pass: 'password123'
+          }
+        }
+      )
       console.log(response);
     } catch (error) {
       console.log(error);
     }
   }
   return (
-    
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -26,7 +38,7 @@ function App() {
           className="App-link"
         >
           <button onClick={postRegister}> Learn React </button>
-          
+
         </div>
       </header>
     </div>
