2c81df166aad4519444ffc304bfc61968c17805a
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-19 17:02:25 +0300

    up

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Cars from './Views/Cars/Cars'
+import Header from './Components/Navigation/Header';
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <Link to="/cars"> Cars </Link>
-          </ul>
-        </nav>
+        <Header/>
         <Routes>
-          <Route path="/cars" element={<Cars/>} />
+          <Route path="/cars" element={<Cars />} />
         </Routes>
       </div>
     </Router>
--- src/Components/Navigation/Header.js
+++ src/Components/Navigation/Header.js
@@ -1,1 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+function Header() {
+    return (
+    <nav>
+        <ul>
+            <Link to="/cars"> Cars </Link>
+        </ul>
+    </nav>
+    )
+}
+export default Header;
