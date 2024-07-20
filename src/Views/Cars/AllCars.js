ca64434f6b593105bdd8d26395f0f22e75317075
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-20 23:44:07 +0300

    up

--- src/Views/Cars/AllCars.js
+++ src/Views/Cars/AllCars.js
@@ -1,30 +1,31 @@
 import React from "react";
-
-function AllCars() {
-
-    const showCars = async () =>{    
-    try {
-
-        const response = await fetch('https://localhost:7290/cars',
-          {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-              'Accept': '*/*'
-            }, 
-          }
-        )
-        console.log(response);
-      } catch (error) {
-        console.log(error);
-      }
+const showCars = async () =>{    
+  try {
+      const response = await fetch('https://localhost:7290/cars',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': '*/*'
+          }, 
+        }
+      )
+      console.log(response);
+    } catch (error) {
+      console.log(error);
     }
-   
+  }
+function AllCars() {
+ 
     return(
         
         <div>
            <h2>All cars</h2>
-            
+            {response.map((car, index) => (
+              <div key={index}>
+                {car.model}
+              </div>
+            ))}
            <button onClick={showCars}>Show</button>
         </div>
     )
