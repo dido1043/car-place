a2c5bedda9a67519a087921b3fc5ba2290e1bf06
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-21 00:00:04 +0300

    up

---
+++ src/Components/CarsCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+function CarCard({car}) {
+    <div>
+        {car.make},
+        {car.model}
+    </div>
+    
+}
+export default CarCard
--- src/Views/Cars/AllCars.js
+++ src/Views/Cars/AllCars.js
@@ -1,34 +1,42 @@
-import React from "react";
-const showCars = async () =>{    
-  try {
+import React, { useState } from "react";
+import CarCard from "../../Components/CarsCard";
+
+function AllCars() {
+  const [cars, setCars] = useState([]);
+
+  const showCars = async () => {
+    try {
       const response = await fetch('https://localhost:7290/cars',
         {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
-            'Accept': '*/*'
-          }, 
+            'Accept': '/'
+          },
         }
       )
-      console.log(response);
+      const jsonData = await response.json();
+      setCars(jsonData)
     } catch (error) {
       console.log(error);
     }
   }
-function AllCars() {
- 
-    return(
-        
-        <div>
-           <h2>All cars</h2>
-            {response.map((car, index) => (
-              <div key={index}>
-                {car.model}
-              </div>
-            ))}
-           <button onClick={showCars}>Show</button>
+
+
+  return (
+
+    <div>
+      <h2>All cars</h2>
+      {cars.map((car, index) => (
+        <div key={index} >
+          <CarCard car={car} />
         </div>
-    )
+        
+      ))}
+      <button onClick={showCars}>Show</button>
+    </div>
+    
+  )
 }
 
 export default AllCars
