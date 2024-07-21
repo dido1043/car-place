96698d3323d161dfb978ce610704a51bf5c0c3c3
veysel-ruskov <77992122+veysel-ruskov@users.noreply.github.com>
2024-07-21 18:50:19 +0300

    feature/1: Updated prettier and eslint

--- src/Components/Navigation/Header.js
+++ src/Components/Navigation/Header.js
@@ -7,7 +7,7 @@ import logoLeft from '../../assests/images/logo2.webp';
 function Header() {
 
     const menuItems = [
-        {   
+        {
             path: "/cars",
             name: "Cars"
         },
@@ -20,18 +20,18 @@ function Header() {
             name: "Register"
         },
     ]
-    
+
     return (
         <nav className="menu">
             <img src={logoLeft} alt="Logo" className="logo-left" />
             <img src={logoRight} alt="Logo" className="logo-right" />
-                {menuItems.map(item => (
-                    <li key={item.path}>
-                        <Link to={item.path} className="menu-item">
-                            {item.name}
-                        </Link>
-                    </li>
-                ))}
+            {menuItems.map(item => (
+                <li key={item.path}>
+                    <Link to={item.path} className="menu-item">
+                        {item.name}
+                    </Link>
+                </li>
+            ))}
         </nav>
     )
 }
