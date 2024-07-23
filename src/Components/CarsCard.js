05850e41200cb3fbc180c7ece880f2476e3988cf
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-23 19:36:31 +0300

    new html structure

--- src/Components/CarsCard.js
+++ src/Components/CarsCard.js
@@ -1,29 +1,25 @@
 import React from "react";
+import '../assests/scss/card.scss'
 function CarCard({ car }) {
     return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>{car.make}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{car.model}</td>
-                    </tr>
-                    <tr>
-                        <td><img src={car.imageUrl} className="" /></td>
-                    </tr>
-                    <tr>
-                        <td>{car.price}$</td>
-                    </tr>
-                    <tr>
-                        <td>{car.year}</td>
-                    </tr>
-                </tbody>
+        <div className="container">
+            <div className="car-card">
+                <div className="car-tumb">
+                    <img src={car.imageUrl} className="" />
+                </div>
+                <div className="car-details">
 
-            </table>
+                    <h4><a href="">{car.make}</a></h4>
+                    <p>{car.model}</p>
+                    <div className="car-bottom-details">
+                        <div className="car-price">Price:{car.price}$</div>
+                        <div className="car-price">Year:{car.year}</div>
+                        <div className="car-links">
+                            <a href=""><i className="fa fa-heart"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
