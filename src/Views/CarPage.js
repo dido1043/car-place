3643eff9457bc89a5abfa338f2faccd0a6ff0737
veysel-ruskov <77992122+veysel-ruskov@users.noreply.github.com>
2024-07-23 20:32:11 +0300

    master: Updated

--- src/Views/CarPage.js
+++ src/Views/CarPage.js
@@ -1,52 +1,54 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useLocation } from 'react'
 import { useParams } from 'react-router-dom';
 import axios from 'axios'
 import BaseButton from '../Components/Shared/BaseButton';
+import CarCard from "../Components/CarsCard"
 
-const CarPage = ({ car }) => {
+
+const CarPage = () => {
     const [formData, setFormData] = useState({
         make: '',
         model: '',
         price: '',
         year: ''
     });
+    const location = useLocation();
+    const { car } = location.state || {};
 
-    useEffect(() => {
-        const deleteCar = (id) => {
-            try {
-                const response = axios.delete(`${process.env.REACT_APP_API_KEY}/cars/delete/${id}`, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': '*/*'
-                    }
-                })
-                console.log(response);
+    const deleteCar = (id) => {
+        try {
+            const response = axios.delete(`${process.env.REACT_APP_API_KEY}/cars/delete/${id}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*'
+                }
+            })
+            console.log(response);
 
-            } catch (error) {
-                console.log(error);
-            }
+        } catch (error) {
+            console.log(error);
         }
+    }
 
-        const editCar = (id) => {
-            try {
-                const response = axios.put(`${process.env.REACT_APP_API_KEY}/cars/edit/${id}`, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Accept': '*/*'
-                    }
-                })
-                console.log(response);
+    const editCar = (id) => {
+        try {
+            const response = axios.put(`${process.env.REACT_APP_API_KEY}/cars/edit/${id}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': '*/*'
+                }
+            })
+            console.log(response);
 
-            } catch (error) {
-                console.log(error);
-            }
+        } catch (error) {
+            console.log(error);
         }
-        //setFormData(car)
-    }, [])
-    
+    }
+    //setFormData(car)
+
     return (
         <div>
-
+            <CarCard car={car} />
             <BaseButton onClick={editCar()} text="Edit"> </BaseButton>
             <BaseButton onClick={deleteCar()} text="Delete"> </BaseButton>
         </div>
