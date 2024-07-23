1305829e9368419958ce591c04d7265f0a44f846
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-23 19:36:37 +0300

    up

--- src/Views/AllCars.js
+++ src/Views/AllCars.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react"
 import CarCard from "../Components/CarsCard"
 import axios from 'axios'
 
-function AllCars() {
+ function AllCars() {
   const [cars, setCars] = useState([])
 
   useEffect(() => {
@@ -24,17 +24,20 @@ function AllCars() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center">
-      <h2 className="text-xl font-bold mb-4">All cars</h2>
-      {cars.length > 0 ? (
-        cars.map((car, index) => (
-          <div key={index} className="m-4"> 
-            <CarCard car={car} />
-          </div>
-        ))
-      ) : (
-        <p>No cars available.</p>
-      )}
+    <div className="container ">
+      <h2 className="text-3xl font-bold mb-4 text-center	">All cars</h2>
+      <div className="flex flex-row justify-center">
+        
+        {cars.length > 0 ? (
+          cars.map((car, index) => (
+            <div key={index} className="m-4">
+              <CarCard car={car} />
+            </div>
+          ))
+        ) : (
+          <p>No cars available.</p>
+        )}
+      </div>
     </div>
   );
 }
