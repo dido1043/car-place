1b7cf6252ce6dc416da554d08f450246239018f1
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-28 21:16:10 +0300

    Added views

--- src/Views/Cars.js
+++ src/Views/Cars.js
@@ -2,7 +2,7 @@ import React from "react";
 function Cars() {
 
     return(
-        <h1>Car Place</h1>
+        <h1 className="text-6xl font-bold mb-4 text-center">Car Place</h1>
     );
 }
 export default Cars;
