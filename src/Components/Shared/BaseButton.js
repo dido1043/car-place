88f9cd8e30140516e178f91babd2d9600e484146
veysel-ruskov <77992122+veysel-ruskov@users.noreply.github.com>
2024-07-29 20:55:15 +0300

    master: Updated

--- src/Components/Shared/BaseButton.js
+++ src/Components/Shared/BaseButton.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "../../assests/scss/Shared/button.scss"
 
-function BaseButton({ text, type }) {
+function BaseButton({ text, type, onClick }) {
 
     return (
-        <button className="button mt-2 mb-2" type={type}>{text}</button>
+        <button className="button mt-2 mb-2" onClick={onClick} type={type}>{text}</button>
     );
 }
 
