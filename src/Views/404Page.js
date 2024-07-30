bcbfe3a8b1a9b869bf2e340248a9b774cfb70199
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-07-30 20:58:43 +0300

    up

--- src/Views/404Page.js
+++ src/Views/404Page.js
@@ -1,4 +1,18 @@
-function NotFoundPage(params) {
-    
+import '../assests/scss/404Page.scss'
+function NotFoundPage() {
+    return (
+        <div class="container">
+            <div class="copy-container center-xy">
+                <p>
+                    404, page not found.
+                </p>
+                <span class="handle"></span>
+
+            </div>
+        </div>
+
+
+
+    )
 }
 export default NotFoundPage
