c54c81273835d41c3a6430a060ee007649d26a15
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-08-01 21:17:46 +0300

    up

---
+++ src/Views/NotFoundPage.js
@@ -0,0 +1,14 @@
+import '../assests/scss/404Page.scss'
+function NotFoundPage() {
+    return (
+        <div>
+            <div class="error">404</div>
+            <br /><br />
+            <span class="info">Page not found</span>
+            <img src="http://images2.layoutsparks.com/1/160030/too-much-tv-static.gif" class="static" />
+        </div>
+
+
+    )
+}
+export default NotFoundPage
