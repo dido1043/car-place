98038d836efd8a8b61e6b405876f0c3f993ac0e9
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-08-26 19:31:29 +0300

    Added Home page

---
+++ src/Views/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+
+    return (
+        <>
+            <h1 className="text-6xl font-bold mb-4 text-center">Car Place</h1>
+            <p className="font-bold mb-4 text-center">Here you can find your future car.</p>
+        </>
+    );
+}
+export default Home
