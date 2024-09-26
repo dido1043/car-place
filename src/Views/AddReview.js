e1930705566af9e811308c623e59e0c59c73c327
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-09-26 17:48:15 +0300

    up

--- src/Views/AddReview.js
+++ src/Views/AddReview.js
@@ -1,8 +1,10 @@
+import AddReviewForm from "../Components/Forms/AddReviewForm";
+
 function AddReview() {
     return (
         <div>
             <h1>Add review</h1>
-            
+            <AddReviewForm/>
         </div>
     );
 
