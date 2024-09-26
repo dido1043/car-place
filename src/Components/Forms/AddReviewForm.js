daeab2f8a965ba041fe1e1e52daeeed91fd15852
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-09-26 17:48:11 +0300

    up

--- src/Components/Forms/AddReviewForm.js
+++ src/Components/Forms/AddReviewForm.js
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 function AddReviewForm({ isEditable, reviewData }) {
-    const [reviewData, setReviewData] = useState({
+    const [reviewFormData, setReviewFormData] = useState({
         carId: 0,
         content: '',
         rating: 0,
     });
+    //Take current carId from the page of the car (url)!!!
     useEffect(() => {
+        const pathParts = window.location.pathname.split('/');
+        const carId = pathParts[pathParts.length-1];
+        console.log(carId);
+        
         if (isEditable) {
-            setReviewData({
-               
+            setReviewFormData({  
+                carId: Number(carId),
                 content: reviewData.content,
-                rating: reviewData.rating
-            })
+                rating: reviewData.rating,
+            })  
         }
-    })
+    },[isEditable])
+    let navigate = useNavigate();
+    const [errors, setErrors] = useState({});
+
 
 }
 export default AddReviewForm;
