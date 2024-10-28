5f3cea3c1986c99ceb9d410dbbed8cb0bbc53337
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-10-29 00:36:30 +0200

    up

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,7 +3,7 @@ function Home() {
     return (
         <>
             <h1 className="text-6xl font-bold mb-4 text-center">Car Place</h1>
-            <p className="font-bold mb-4 text-center">Here you can find your future car.</p>
+            <p className="font-bold mb-4 text-center">Here you can find the best car for you and rent it.</p>
         </>
     );
 }
