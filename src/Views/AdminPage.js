5dd6d3cdeafe69df73fca1a5d11b76e2009b001b
Deyan Dudunovski <96482365+dido1043@users.noreply.github.com>
2024-11-13 00:36:04 +0200

    up

--- src/Views/AdminPage.js
+++ src/Views/AdminPage.js
@@ -1,4 +1,17 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import BaseButton from "../Components/Shared/BaseButton";
 function AdminPage() {
-    
+    const nav = useNavigate();
+    const navigateToAllRequests = async () => {
+        await nav("/allCars/cars/rentrequests")
+    }
+    return (
+        <div>
+            <h1>Admin page</h1>
+            <BaseButton text="Rent requests" onClick={navigateToAllRequests}/>
+        </div>
+    )
+
 }
 export default AdminPage;
